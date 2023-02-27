611fa23d4f5d5e1f282076133bfb20e5a04173a0
delfranco78260 <76877535+delfranco78260@users.noreply.github.com>
2023-02-27 14:48:42 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,17 @@
 huskylens.initI2c()
 huskylens.initMode(protocolAlgorithm.ALGORITHM_OBJECT_TRACKING)
-DFRobotMaqueenPlus.I2CInit()
 basic.forever(function () {
     huskylens.request()
-    if (huskylens.readeBox(1, Content1.xCenter) > 80 && huskylens.readeBox(1, Content1.xCenter) == 240) {
+    if (huskylens.readeBox(1, Content1.xCenter) > 80 && huskylens.readeBox(1, Content1.xCenter) < 160) {
         basic.showIcon(IconNames.Heart)
-        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 250)
-    } else if (huskylens.readeBox(1, Content1.xCenter) < 241) {
-        basic.showIcon(IconNames.No)
-        DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 250)
-        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 0)
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 40)
     } else if (huskylens.readeBox(1, Content1.xCenter) < 79) {
+        basic.showIcon(IconNames.No)
+        DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 60)
+        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 0)
+    } else if (huskylens.readeBox(1, Content1.xCenter) > 180) {
         basic.showIcon(IconNames.Yes)
         DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 0)
-        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 250)
+        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 80)
     }
 })
