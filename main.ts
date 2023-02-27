f977eba4ba16727d52cd345304fa862b17ea2e08
delfranco78260 <76877535+delfranco78260@users.noreply.github.com>
2023-02-27 14:06:48 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,18 @@
 huskylens.initI2c()
 huskylens.initMode(protocolAlgorithm.ALGORITHM_OBJECT_TRACKING)
+DFRobotMaqueenPlus.I2CInit()
 basic.forever(function () {
     huskylens.request()
     if (huskylens.readeBox(1, Content1.xCenter) > 80 && huskylens.readeBox(1, Content1.xCenter) == 240) {
-        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 70)
+        basic.showIcon(IconNames.Heart)
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 250)
     } else if (huskylens.readeBox(1, Content1.xCenter) < 241) {
-        DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 70)
+        basic.showIcon(IconNames.No)
+        DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 250)
         DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 0)
     } else if (huskylens.readeBox(1, Content1.xCenter) < 79) {
+        basic.showIcon(IconNames.Yes)
         DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 0)
-        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 70)
-    } else {
-        DFRobotMaqueenPlus.mototStop(Motors.ALL)
+        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 250)
     }
 })
